904bfc0c0711be566bbeb398090f5ba001c02283
Douglas <douglas.sousa@ccc.ufcg.edu.br>
2022-05-11 17:39:20 -0300

    fixed solution to b2 on contest 789

--- contests/round789/B.cpp
+++ contests/round789/B.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef pair<int,int> pii;
+
 int main(){
 
     int t;
@@ -14,21 +16,24 @@ int main(){
         string s;
         cin >> s;
 
-        int f[n][2];
+        pii f[n][2];
 
-        f[0][0] = s[0]=='0'?0:1;
-        f[0][1] = s[0]=='0'?1:0;
+        f[0][0] = {s[0]=='0'?0:1,1};
+        f[0][1] = {s[0]=='0'?1:0,1};
         
         for(int i=1;i<n;i++){
             for(int j=0;j<2;j++){
                 int change = s[i]-'0'==j?0:1;
-                int r1 = f[i-1][j] + change;
-                int r2 = i%2==0?f[i-1][1-j] + change:INT_MAX;
+                pii continua = f[i-1][j];
+                pii quebra = f[i-1][1-j];
+                pii r1 = {continua.first + change, continua.second};
+                pii r2 = {i%2==0?quebra.first + change:INT_MAX, quebra.second + 1};
                 f[i][j] = min(r1, r2);
             }
         }
 
-        cout << min(f[n-1][0], f[n-1][1]) << endl;
+        pii resposta = min(f[n-1][0], f[n-1][1]);
+        cout << resposta.first << " " << resposta.second << endl;
 
     }
 
