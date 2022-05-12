7baa3c5e89898f27761e2a8f538008c734dd0545
Douglas <douglas.sousa@ccc.ufcg.edu.br>
2022-05-12 16:53:35 -0300

    added solution to fenwicktree1d

---
+++ topics/data structures/fenwick tree/fenwicktree1d.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 100010;
+int BIT[N];
+
+void add(int v, int i){
+    for(;i<=N;i=(i|(i-1))+1){
+        BIT[i] += v;
+    } 
+}
+
+int sum(int i){
+    int answer = 0;
+    for(;i>0;i=i&(i-1)) answer += BIT[i]; 
+    return answer;
+}
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    int a[n+1];
+    for(int i=1;i<=n;i++){
+        cin >> a[i];
+        add(a[i],i);
+    }
+    
+    cout << "1-indexed fenwick tree\nTREE = [ ";
+    for(int i=1;i<=n;i++){
+        cout << BIT[i] << " ";
+    }
+    cout << "]\n";
+
+    return 0;
+}
